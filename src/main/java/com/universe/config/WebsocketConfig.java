f3667dfb2f89fd9adae718145009052c17e76111
liuyalou <liuyalou@outlook.com>
2023-04-27 01:41:49 +0800

    feat: 新增websocket配置类

---
+++ src/main/java/com/universe/config/WebsocketConfig.java
@@ -0,0 +1,12 @@
+package com.universe.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Nick Liu
+ * @date 2023/4/27
+ */
+@Configuration
+public class WebsocketConfig {
+
+}
